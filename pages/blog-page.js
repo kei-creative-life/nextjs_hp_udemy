730cf677a954723c5a74263fbcada828a3dd2980
KEI <68884990+kei1992@users.noreply.github.com>
2021-02-09 22:21:39 +0900

    create-layout-component

---
+++ pages/blog-page.js
@@ -0,0 +1,7 @@
+import Layout from "../components/layout";
+
+const Blog = () => {
+  return <Layout title="Blog">Blog Page</Layout>;
+};
+
+export default Blog;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
-    <div className="min-h-screen px-2 py-0 flex flex-col justify-center items-center">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>Hello Next.js</h1>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Next.js</p>
+    </Layout>
   );
 }
